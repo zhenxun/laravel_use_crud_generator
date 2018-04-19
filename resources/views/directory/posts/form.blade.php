50cdd46a8a89ac6e4acd78041d0b39bfd64eddc2
Zhen-Xun WONG <wzx@playsure.tw>
2018-04-19 17:35:09 +0800

    This Generator package provides various generators like CRUD, API, Controller, Model, Migration, View for your painless development of your applications.

---
+++ resources/views/directory/posts/form.blade.php
@@ -0,0 +1,19 @@
+<div class="form-group {{ $errors->has('title') ? 'has-error' : ''}}">
+    <label for="title" class="col-md-4 control-label">{{ 'Title' }}</label>
+    <div class="col-md-6">
+        <input class="form-control" name="title" type="text" id="title" value="{{ $post->title or ''}}" >
+        {!! $errors->first('title', '<p class="help-block">:message</p>') !!}
+    </div>
+</div><div class="form-group {{ $errors->has('body') ? 'has-error' : ''}}">
+    <label for="body" class="col-md-4 control-label">{{ 'Body' }}</label>
+    <div class="col-md-6">
+        <textarea class="form-control" rows="5" name="body" type="textarea" id="body" >{{ $post->body or ''}}</textarea>
+        {!! $errors->first('body', '<p class="help-block">:message</p>') !!}
+    </div>
+</div>
+
+<div class="form-group">
+    <div class="col-md-offset-4 col-md-4">
+        <input class="btn btn-primary" type="submit" value="{{ $submitButtonText or 'Create' }}">
+    </div>
+</div>
